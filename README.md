8a16b3c70e25b99b720ff3493b7e947fdccaa091
Luke Parker <lukeparker5132@gmail.com>
2024-12-28 03:23:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zephyr Verify Bulletproofs
+
+This is an independent verification of the claims within
+https://x.com/demand_answer/status/1868950596461772922. While there could be a
+flaw in the methodology present here, it would appear to confirm the
+allegations.
---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use serde::Deserialize;
+use serde_json::json;
+
+use monero_rpc::Rpc;
+use monero_serai::{
+  io::*,
+  ringct::{clsag::Clsag, bulletproofs::Bulletproof},
+};
+
+#[tokio::main]
+async fn main() {
+  let tx_hashes = [
+    // The transactions alleged to have invalid Bulletproofs
+    "b17ef3d2e65ab980c8bd6cbe3210a8b3c9e417fb9621209380f7c1f756fcb2ed",
+    "4e7ea2cc5484508bd17ff43f3fb8fbd69a22062f24a869794c513d89aee3eb6d",
+    "d3abcf2bc0a173d6d8af19d88b2d7d610385abf258590d24ca82fc586017c6dd",
+    // A transaction made earlier on the same day, which also isn't a conversion, not alleged to
+    // have invalid Bulletproofs
+    "784d3e756aed645606f74aa3519ecf96e137a025c22c9cc585a6d4ec0fdb651a",
+    // A transaction made later on the same day, which also isn't a conversion, not alleged to have
+    // invalid Bulletproofs
+    "98b7180424495e3388c11b0ee9b4218018b06096b03ebde61467a4e9a49ddc79",
+  ];
+
+  let txs: Vec<Vec<u8>> = {
+    let rpc =
+      monero_simple_request_rpc::SimpleRequestRpc::new("http://node.zeph.network".to_string())
+        .await
+        .unwrap();
+
+    #[derive(Debug, Deserialize)]
+    struct TransactionResponse {
+      tx_hash: String,
+      as_hex: String,
+    }
+    #[derive(Debug, Deserialize)]
+    struct TransactionsResponse {
+      #[serde(default)]
+      missed_tx: Vec<String>,
+      txs: Vec<TransactionResponse>,
+    }
+    let txs: TransactionsResponse = rpc
+      .rpc_call(
+        "get_transactions",
+        Some(json!({
+        "txs_hashes": tx_hashes,
+        })),
+      )
+      .await
+      .unwrap();
+    assert!(txs.missed_tx.is_empty());
+
+    txs
+      .txs
+      .into_iter()
+      .enumerate()
+      .map(|(i, tx)| {
+        assert_eq!(tx_hashes[i], tx.tx_hash);
+        hex::decode(&tx.as_hex).unwrap()
+      })
+      .collect()
+  };
+
+  for (tx_hash, tx) in tx_hashes.into_iter().zip(txs) {
+    let mut tx = tx.as_slice();
+    let tx = &mut tx;
+
+    // Prefix:
+    // - version
+    // - unlock_time
+    // - vin
+    // - vout
+    // - extra
+    // - pricing_record_height (varint)
+    // - amount_burnt (varint)
+    // - amount_minted (varint)
+    let version = read_varint::<_, u64>(tx).unwrap();
+    assert_eq!(version, 3);
+    let _unlock_time = read_varint::<_, u64>(tx).unwrap();
+    let inputs = read_varint::<_, u64>(tx).unwrap();
+    for _ in 0 .. inputs {
+      // TxIn:
+      // - type
+      // - amount
+      // - asset_type
+      // - key_offsets
+      // - key_image
+      let kind = read_byte(tx).unwrap();
+      assert_eq!(kind, 2);
+      let _amount = read_varint::<_, u64>(tx).unwrap();
+      let asset_type = read_vec(read_byte, tx).unwrap();
+      let _key_offsets = read_vec(read_varint::<_, u64>, tx).unwrap();
+      assert_eq!(&asset_type, b"ZEPH");
+      let _key_image = read_point(tx).unwrap();
+    }
+    let outputs = read_varint::<_, u64>(tx).unwrap();
+    for _ in 0 .. outputs {
+      // TxOut:
+      // - amount
+      // - type
+      // - key
+      // - asset_type
+      // - view_tag
+      let _amount = read_varint::<_, u64>(tx).unwrap();
+      let kind = read_byte(tx).unwrap();
+      assert_eq!(kind, 2);
+      let _key = read_point(tx).unwrap();
+      let asset_type = read_vec(read_byte, tx).unwrap();
+      assert_eq!(&asset_type, b"ZEPH");
+      let _view_tag = read_byte(tx).unwrap();
+    }
+    let _extra = read_vec(read_byte, tx).unwrap();
+    let _pricing_record_height = read_varint::<_, u64>(tx).unwrap();
+    let amount_burnt = read_varint::<_, u64>(tx).unwrap();
+    let amount_minted = read_varint::<_, u64>(tx).unwrap();
+    assert_eq!(amount_burnt, 0);
+    assert_eq!(amount_minted, 0);
+
+    // Base:
+    // - type
+    // - txnFee
+    // - ecdhInfo
+    // - outPk
+    let kind = read_byte(tx).unwrap();
+    assert_eq!(kind, 6);
+    let _fee = read_varint::<_, u64>(tx).unwrap();
+    let _ecdh_info = read_raw_vec(read_u64, outputs.try_into().unwrap(), tx).unwrap();
+    let output_commitments = read_raw_vec(read_point, outputs.try_into().unwrap(), tx).unwrap();
+
+    // Prunable:
+    // - Option<BP+>
+    // - CLSAGs
+    // - Pseudo-outs
+    let bp_present = read_byte(tx).unwrap();
+    assert_eq!(bp_present, 1);
+    let bp = Bulletproof::read_plus(tx).unwrap();
+
+    const RING_LEN: usize = 16;
+    let _clsags =
+      read_raw_vec(|r| Clsag::read(RING_LEN, r), inputs.try_into().unwrap(), tx).unwrap();
+    let _pseudo_outs = read_raw_vec(read_point, inputs.try_into().unwrap(), tx).unwrap();
+    assert!(tx.is_empty());
+
+    let valid = bp.verify(&mut rand_core::OsRng, &output_commitments);
+    println!("{tx_hash} has valid Bulletproofs: {valid}");
+  }
+}
